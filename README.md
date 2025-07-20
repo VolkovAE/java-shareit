96d9441404ae4dda2a14660ddee96d5f80072c1f
VolkovA <va771@yandex.ru>
2025-07-20 19:30:32 +0300

    Подготовить проект. Создание базы данных.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # java-shareit
 Template repository for Shareit project.
+
+Shareit Documentation
+
+# Описание структуры базы данных
+
+**ER-диаграмма базы данных**
+
+![link](/ER-diagram%20DB.png)
+
+\
+<span style="color:yellow">**todo** приложите примеры запросов для основных операций вашего приложения!!!</span>
--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,12 +2,14 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration(classes = ShareItApp.class)
 @SpringBootTest
 class ShareItTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
