e52611e0bc1b2d827789416623f5417f96be88d5
VolkovA <va771@yandex.ru>
2025-09-01 21:36:46 +0300

    Черновик тестов

--- server/src/test/java/ru/practicum/shareit/ShareItServerAppTest.java
+++ server/src/test/java/ru/practicum/shareit/ShareItServerAppTest.java
@@ -12,15 +12,15 @@ import ru.practicum.shareit.request.dto.NewRequestItem;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //@ContextConfiguration(classes = ShareItServerApp.class)
-@SpringBootTest()//webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.NONE)//webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 //@Import({NewRequestItem.class})
 public class ShareItServerAppTest {
     private final EntityManager em;
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
     @Test
     public void add(NewRequestItem requestItem, Long userId) {
