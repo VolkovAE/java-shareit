4428d0baca5fd3303de7a063fe72da5fe406e35a
VolkovA <va771@yandex.ru>
2025-09-02 18:14:40 +0000

    Сделал юнит и интеграционные тесты сервисов user, item, request, booking.

--- server/src/test/java/ru/practicum/shareit/ServerJUnitAndITests.java
+++ server/src/test/java/ru/practicum/shareit/ServerJUnitAndITests.java
@@ -10,14 +10,15 @@ import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.NewBookingRequest;
 import ru.practicum.shareit.booking.model.StatusBooking;
 import ru.practicum.shareit.booking.service.BookingServiceImpl;
+import ru.practicum.shareit.exception.AccessForbidden;
 import ru.practicum.shareit.exception.NotFoundException;
-import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.dto.ItemWithDateDto;
-import ru.practicum.shareit.item.dto.NewItemRequest;
-import ru.practicum.shareit.item.dto.UpdateItemRequest;
+import ru.practicum.shareit.item.dto.*;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.service.ItemDBServiceImpl;
 import ru.practicum.shareit.item.storage.ItemRepository;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.dto.NewRequestItem;
+import ru.practicum.shareit.request.service.ItemRequestServiceImpl;
 import ru.practicum.shareit.user.dto.NewUserRequest;
 import ru.practicum.shareit.user.dto.UpdateUserRequest;
 import ru.practicum.shareit.user.dto.UserDto;
@@ -26,6 +27,8 @@ import ru.practicum.shareit.user.service.UserDBServiceImpl;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -58,6 +61,9 @@ public class ServerJUnitAndITests {
 
     @Autowired
     private final BookingServiceImpl bookingService;
+
+    @Autowired
+    private final ItemRequestServiceImpl requestService;
 
     @Test
     void contextLoads() {
@@ -254,23 +260,31 @@ public class ServerJUnitAndITests {
         assertNotNull(bookingDto.getId(), "Ошибка при записи новой заявки на аренду: Возвращено NULL в ID.");
 
         // Проверяем утверждение заявки владельцем вещи.
+        // Проверим, что если пользователь не владелец вещи, то будет исключение.
+        Long bookingId = bookingDto.getId();
 
+        AccessForbidden thrown = assertThrows(AccessForbidden.class, () -> {
+            bookingService.approve(bookingId, userDtoBooker.getId(), true);
+        }, "Не выброшено исключение при одобрении заявки не владельцем вещи.");
 
-        //        BookingDto approve(Long bookingId, Long userId, boolean approval);
+        bookingDto = bookingService.approve(bookingId, userDtoOwner.getId(), true);
 
+        assertEquals(StatusBooking.APPROVED, StatusBooking.fromString(bookingDto.getStatus()), "Ошибка при утверждении заявки владельцем вещи: Не верно записан статус заявки.");
 
         // Проверяем поиск заявки по id.
+        bookingDto = bookingService.findById(bookingId, userDtoBooker.getId());
 
-        //        BookingDto findById(Long bookingId, Long userId);
-
+        assertEquals(bookingId, bookingDto.getId(), "Ошибка при получении данных по заявке с id: Возвращена не та заявка.");
 
         // Проверяем поиск заявок арендатора.
-        //        Collection<BookingDto> findByBookerOrOwner(Long userId, String state, Boolean isOwner);
+        Collection<BookingDto> bookingDtoCollection = bookingService.findByBookerOrOwner(userDtoBooker.getId(), "ALL", false);
+        assertEquals(1, bookingDtoCollection.size(), "Ошибка при получении заявок арендатора: Не верно получен список заявок.");
+        assertEquals(bookingId, bookingDtoCollection.stream().toList().getFirst().getId(), "Ошибка при получении заявок арендатора: Состав заявок не верен.");
 
         // Проверяем поиск заявок на вещи владельца.
-
-        //        Collection<BookingDto> findByBookerOrOwner(Long userId, String state, Boolean isOwner);
-
+        bookingDtoCollection = bookingService.findByBookerOrOwner(userDtoOwner.getId(), "ALL", true);
+        assertEquals(1, bookingDtoCollection.size(), "Ошибка при получении заявок на вещи владельца: Не верно получен список заявок.");
+        assertEquals(bookingId, bookingDtoCollection.stream().toList().getFirst().getId(), "Ошибка при получении заявок на вещи владельца: Состав заявок не верен.");
     }
 
     private NewBookingRequest getNewBookingRequest(Long itemId) {
@@ -287,11 +301,111 @@ public class ServerJUnitAndITests {
         return newBookingRequest;
     }
 
-//    // Проверяем добавление комментария (item).
-//    itemRequest = getNewItemRequest(true);
-//
-//    itemDto = itemService.add(itemRequest, userDto.getId());
+    @Test
+    public void testServiceRequest() {
+        // Подготовим букера.
+        NewUserRequest userRequestBooker = getNewUserRequest();
 
-    //        CommentDto addComment(Long itemId, Long userId, NewCommentRequest commentRequest);
+        UserDto userDtoBooker = userService.add(userRequestBooker);
 
+        // Проверим размещение запроса на вещь.
+        NewRequestItem newRequestItem = getNewRequestItem();
+
+        ItemRequestDto itemRequestDto = requestService.add(newRequestItem, userDtoBooker.getId());
+
+        assertEquals(newRequestItem.getDescription(), itemRequestDto.getDescription(), "Ошибка при размещении запроса на вещь: Не верно записано наименование.");
+        assertTrue(LocalDateTime.now().isAfter(itemRequestDto.getCreated()) || LocalDateTime.now().equals(itemRequestDto.getCreated()),
+                "Ошибка при размещении запроса на вещь: Не верно определено дата и время запроса.");
+        assertNotNull(itemRequestDto.getId(), "Ошибка при размещении запроса на вещь: Возвращено NULL в ID.");
+
+        // Проверяем получение моих запросов.
+        Collection<ItemRequestDto> itemRequestDtoCollection = requestService.findAllMy(userDtoBooker.getId());
+        assertEquals(1, itemRequestDtoCollection.size(), "Ошибка при получении моих запросов: Размер списка запросов не верен.");
+        assertEquals(itemRequestDto.getId(), itemRequestDtoCollection.stream().toList().getFirst().getId(), "Ошибка при получении моих запросов: Состав запросов не верен.");
+
+        // Проверяем получение чужих запросов. Подготовим второго букера.
+        NewUserRequest userRequestBooker2 = getNewUserRequest();
+
+        UserDto userDtoBooker2 = userService.add(userRequestBooker2);
+
+        itemRequestDtoCollection = requestService.findAllOther(userDtoBooker2.getId());
+        assertEquals(1, itemRequestDtoCollection.size(), "Ошибка при получении чужих запросов: Размер списка запросов не верен.");
+
+        // Проверяем получение запроса по id.
+        ItemRequestDto itemRequestDtoById = requestService.findById(itemRequestDto.getId());
+        assertEquals(itemRequestDto.getId(), itemRequestDtoById.getId(), "Ошибка при получении запроса по id: Возвращен не тот запрос.");
+
+        // Проверим добавление вещи под запрос.
+        // Владелец вещи.
+        NewUserRequest userRequestOwner = getNewUserRequest();
+        UserDto userDtoOwner = userService.add(userRequestOwner);
+
+        // Вещь 1 по запросу.
+        NewItemRequest newItemRequest1 = getNewItemRequest(true);
+        newItemRequest1.setRequestId(itemRequestDto.getId());
+        ItemDto itemDto1 = itemService.add(newItemRequest1, userDtoOwner.getId());
+
+        // Вещь 2 по запросу.
+        NewItemRequest newItemRequest2 = getNewItemRequest(true);
+        newItemRequest2.setRequestId(itemRequestDto.getId());
+        ItemDto itemDto2 = itemService.add(newItemRequest2, userDtoOwner.getId());
+
+        itemRequestDtoById = requestService.findById(itemRequestDto.getId());
+        List<ItemOnRequestDto> itemOnRequestDtoList = itemRequestDtoById.getItems();
+
+        assertEquals(2, itemOnRequestDtoList.size(), "Ошибка в определении количества вещей созданных по запросу: Размер списка вещей не верен.");
+        assertEquals(itemDto1.getId(), itemOnRequestDtoList.getFirst().getId(), "Ошибка в определении количества вещей созданных по запросу: Состав вещей не верен (1).");
+        assertEquals(itemDto2.getId(), itemOnRequestDtoList.getLast().getId(), "Ошибка в определении количества вещей созданных по запросу: Состав вещей не верен (2).");
+    }
+
+    private NewRequestItem getNewRequestItem() {
+        NewRequestItem newRequestItem = new NewRequestItem();
+        newRequestItem.setDescription(RandomStringUtils.randomAlphabetic(LENGTH_DESCR_NEW_ITEM));
+        return newRequestItem;
+    }
+
+    @Test
+    public void testComment() throws InterruptedException {
+        // Чтобы добавить коммент, нужно, чтобы пользователь брал вещь в аренду.
+        // Подготовим владельца вещи.
+        NewUserRequest userRequestOwner = getNewUserRequest();
+
+        UserDto userDtoOwner = userService.add(userRequestOwner);
+
+        // Подготовим вещь.
+        NewItemRequest itemRequest = getNewItemRequest(true);
+
+        ItemDto itemDto = itemService.add(itemRequest, userDtoOwner.getId());
+
+        // Подготовим букера.
+        NewUserRequest userRequestBooker = getNewUserRequest();
+
+        UserDto userDtoBooker = userService.add(userRequestBooker);
+
+        // Проверяем добавление заявки.
+        NewBookingRequest newBookingRequest = getNewBookingRequest(itemDto.getId());
+
+        BookingDto bookingDto = bookingService.add(newBookingRequest, userDtoBooker.getId());
+
+        // Владелец вещи утверждает заявку.
+        bookingDto = bookingService.approve(bookingDto.getId(), userDtoOwner.getId(), true);
+
+        // Пауза, чтобы заявка была выполнена.
+        TimeUnit.SECONDS.sleep(20L);
+
+        // Проверяем добавление комментария.
+        String comment = "Рекомендую!";
+
+        NewCommentRequest newCommentRequest = new NewCommentRequest();
+        newCommentRequest.setText(comment);
+
+        CommentDto commentDto = itemService.addComment(itemDto.getId(), userDtoBooker.getId(), newCommentRequest);
+
+        ItemWithDateDto itemWithDateDto = itemService.getById(itemDto.getId(), userDtoOwner.getId());
+        List<CommentDto> commentDtoList = itemWithDateDto.getCommentDtoList();
+
+        assertEquals(1, commentDtoList.size(), "Ошибка при добавлении комментария: Состав комментариев не верен.");
+        assertEquals(commentDto.getText(), commentDtoList.getFirst().getText(), "Ошибка при добавлении комментария: Комментарий не верен.");
+        assertEquals(commentDto.getId(), commentDtoList.getFirst().getId(), "Ошибка при добавлении комментария: Комментарий не тот.");
+    }
 }
